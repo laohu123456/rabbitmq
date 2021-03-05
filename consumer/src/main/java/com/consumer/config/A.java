fd60d8ec66b2e0850844c7be0a2219abd8589b60
h <aaa>
2021-03-05 15:50:12 +0800

    测试添加数据

--- consumer/src/main/java/com/consumer/config/A.java
+++ consumer/src/main/java/com/consumer/config/A.java
@@ -4,4 +4,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
 }
